cd602f5fce6b272ce75423d4918557466453150e
root <yenachar@protonmail.com>
2025-10-17 04:07:35 +0000

    Updated to host domain names, with playground.verdikta.org as an example.

--- example-bounty-program/client/vite.config.js
+++ example-bounty-program/client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      'playground.verdikta.org',
+      // Add any other possible domains 
+    ]
+  }
 })

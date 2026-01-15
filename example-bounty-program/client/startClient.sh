f7a6cbc32cf859c5500a8cf639dd439da73df3af
root <yenachar@protonmail.com>
2026-01-15 03:07:54 +0000

    Run client in the background.

--- example-bounty-program/client/startClient.sh
+++ example-bounty-program/client/startClient.sh
@@ -1,1 +1,9 @@
-npm run dev -- --host
+#!/bin/bash
+cd "$(dirname "$0")"
+nohup npm run dev -- --host 0.0.0.0 > client.log 2>&1 &
+echo $! > client.pid
+echo "Client started with PID $(cat client.pid)"
+
+
+# npm run dev -- --host 0.0.0.0
+

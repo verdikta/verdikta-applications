2f4d620ea0c168af3d298dc2e1f5c1644d64ed76
root <yenachar@protonmail.com>
2026-01-22 04:14:51 +0000

    Changed browser tab title for testnet website.

--- example-bounty-program/client/src/main.jsx
+++ example-bounty-program/client/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { config } from './config'
+
+// Set page title based on network
+document.title = config.network === 'base-sepolia' ? 'Bounties - Testnet' : 'Bounties'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
